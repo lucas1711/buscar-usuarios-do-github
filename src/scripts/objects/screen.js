090f012bd3bbf1c658b06f99852f63fe70ff8a75
lucas costa <lucas34474040@gmail.com>
2022-07-12 11:38:04 -0300

    acrescentando no projeto numero de seguidores e seguidos do usuario

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -4,25 +4,35 @@ const screen = {
         this.userProfile.innerHTML = `<div class="info">
                                     <img src="${user.avatarUrl}" alt= "foto do perfil do usuário"/>
                                      <div class="data">
-                                          <h2>${user.name }</h2>
-                                          <p>${user.bio }</p>
+                                          <h2>${user.name}</h2>
+                                          <p>${user.bio}</p>
                                      </div>
-                                     </div>`
+                                     <div class="counters">
+                                        <div class="followers">
+                                            <h4>👥 Seguidores</h4>
+                                            <span>${user.followers ?? 'não possui seguidores'}</span>
+                                     </div>
+                                     <div class="following">
+                                        <h4>👥 Seguindo</h4>
+                                        <span>${user.following ?? 'não possui seguidores'}</span>
+                                     </div>
+                                 </div>
+                              </div>`
 
         let repositoriesItens = " "
-        user.repositories.forEach( repo => repositoriesItens +=  `<li><a href="${repo.html_url}"
+        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}"
         target="_blank">${repo.name}</a></li>`)
-        
 
-        if(user.repositories.length > 0){
+
+        if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class="repositories section">
                                            <h2>Repositórios</h2>
                                            <ul>${repositoriesItens}</ul>
                                            </div>`
         }
     },
-    renderNotFound(){
-        this.userProfile.innerHTML = "<h3> Usuário não encontrado </h3>" 
+    renderNotFound() {
+        this.userProfile.innerHTML = "<h3> Usuário não encontrado </h3>"
     }
 }
 
